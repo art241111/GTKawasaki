fec3918cba9bbb636dd8142bde1d3e737cf166a0
artem_admin <art241111@mail.ru>
2020-08-21 12:42:27 +0300

    Add documentation at AmountOfMovement.kt

--- app/src/main/java/ru/art241111/graphicaltoolforkawasaki/view/util/AmountOfMovement.kt
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/view/util/AmountOfMovement.kt
@@ -1,5 +1,9 @@
 package ru.art241111.graphicaltoolforkawasaki.view.util
 
+/**
+ * The option of speed of movement of the robot
+ * @author Artem Gerasimov
+ */
 enum class AmountOfMovement {
     FAST,
     SLOW
