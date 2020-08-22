cc3239be8a16d408b1e3dadc9c43842d2045bf6b
artem_admin <art241111@mail.ru>
2020-08-22 12:15:28 +0300

    Add documentation in protocols for recycler view

--- app/src/main/java/ru/art241111/graphicaltoolforkawasaki/configuringRv/adapters/protocols/ItemTouchHelperAdapter.kt
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/configuringRv/adapters/protocols/ItemTouchHelperAdapter.kt
@@ -1,5 +1,11 @@
 package ru.art241111.graphicaltoolforkawasaki.configuringRv.adapters.protocols
 
+/**
+ * Interface that defines changes when moving and when deleting
+ * items in recycler view.
+ *
+ * @author Artem Gerasimov
+ */
 interface ItemTouchHelperAdapter {
     fun onItemMove(fromPosition: Int, toPosition: Int): Boolean
 
--- app/src/main/java/ru/art241111/graphicaltoolforkawasaki/configuringRv/adapters/protocols/OnItemClickListener.kt
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/configuringRv/adapters/protocols/OnItemClickListener.kt
@@ -1,5 +1,10 @@
 package ru.art241111.graphicaltoolforkawasaki.configuringRv.adapters.protocols
 
+/**
+ * Interface that defines the action when clicking the item
+ *
+ * @author Artem Gerasimov
+ */
 interface OnItemClickListener {
     fun onItemClick(position: Int)
 }
