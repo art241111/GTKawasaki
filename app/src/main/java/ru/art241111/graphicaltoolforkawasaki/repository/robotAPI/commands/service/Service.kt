834f8c22e7fadae664706554453b813069a839c1
artem_admin <art241111@mail.ru>
2020-08-24 14:02:43 +0300

    Add documentation at Service.kt

--- app/src/main/java/ru/art241111/graphicaltoolforkawasaki/repository/robotAPI/commands/service/Service.kt
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/repository/robotAPI/commands/service/Service.kt
@@ -5,20 +5,39 @@ import link.State
 import ru.art241111.graphicaltoolforkawasaki.repository.robotAPI.RobotEntity
 
 class Service(private var robotEntity: RobotEntity) {
+    /**
+     * Turn on robot motors
+     * TODO: Add
+     */
     fun turnOnTheMotors() =
             robotEntity.writer.sendCommand(TURN_ON_THE_MOTORS.command)
 
+    /**
+     * Turn off robot motors
+     * TODO: Add
+     */
     fun turnOffTheMotors() =
             robotEntity.writer.sendCommand(TURN_OFF_THE_MOTORS.command)
 
+    /**
+     * Reset errors
+     * TODO: Add
+     */
     fun resetErrors(): Boolean{
         robotEntity.errors.clear()
         robotEntity.state = State.WAITING_COMMAND
         return robotEntity.writer.sendCommand(DELETE_ERRORS.command)
     }
 
+    /**
+     * Clean command queue
+     */
     fun clearQueue() = robotEntity.writer.cleanQueue()
 
+    /**
+     * Update position
+     * TODO: DO it
+     */
     fun updateInfoAboutPosition() = robotEntity.writer
             .sendCommand(ROBOT_POSITION.command)
 }
