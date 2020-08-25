f8713ee50dad36a5373256107efff568bf8dc959
artem_admin <art241111@mail.ru>
2020-08-25 11:06:33 +0300

    Add spinner expansion: add elements by name

---
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/utils/SpinnerExp.kt
@@ -0,0 +1,15 @@
+package ru.art241111.graphicaltoolforkawasaki.utils
+
+import android.widget.Spinner
+
+// TODO think about how do it differently
+fun Spinner.setSelection(itemName: String){
+    var position = 0
+    for(i in 0 until this.count){
+        val name = this.getItemAtPosition(i)
+        if(this.getItemAtPosition(i).toString() == itemName) {
+            position = i
+        }
+    }
+    this.setSelection(position)
+}
