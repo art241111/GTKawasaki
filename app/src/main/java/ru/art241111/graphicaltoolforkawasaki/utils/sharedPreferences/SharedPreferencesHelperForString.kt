4b996a60bf1ea419b2d1b36511a503d6316867fd
artem_admin <art241111@mail.ru>
2020-08-27 13:45:12 +0300

    Add SharedPreferencesHelperForString.kt

---
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/utils/sharedPreferences/SharedPreferencesHelperForString.kt
@@ -0,0 +1,37 @@
+package ru.art241111.graphicaltoolforkawasaki.utils.sharedPreferences
+
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import ru.art241111.graphicaltoolforkawasaki.utils.sharedPreferences.protocols.LoadFromSharedPreferences
+import ru.art241111.graphicaltoolforkawasaki.utils.sharedPreferences.protocols.SaveSharedPreferences
+import ru.art241111.graphicaltoolforkawasaki.utils.sharedPreferences.protocols.UpdateSharedPreferences
+
+class SharedPreferencesHelperForString( activity: Activity, sharedPreferences: String)
+    :SaveSharedPreferences, LoadFromSharedPreferences, UpdateSharedPreferences {
+
+    private val preferences: SharedPreferences
+            = activity.getSharedPreferences(sharedPreferences, Context.MODE_PRIVATE)
+
+    fun load(preferencesKey: String): String {
+        if(preferences.contains(preferencesKey)) {
+            return preferences.getString(preferencesKey, "").toString()
+        }
+        return ""
+    }
+
+    fun save(preferencesKey: String, newValue: String) {
+        val editor: SharedPreferences.Editor = preferences.edit()
+        editor.putString(preferencesKey, newValue)
+        editor.apply()
+    }
+
+    fun update(oldValue:String,newValue: String, preferencesKey: String): String{
+        return if(oldValue != newValue){
+            save(preferencesKey,newValue)
+            newValue
+        } else{
+            oldValue
+        }
+    }
+}
