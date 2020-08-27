e181e95bc994aad08853bac3b72d36e18472f94f
artem_admin <art241111@mail.ru>
2020-08-27 13:46:08 +0300

    Migrate create connection from DataForLinkFragment.kt to RobotViewModel.kt

--- app/src/main/java/ru/art241111/graphicaltoolforkawasaki/viewModel/RobotViewModel.kt
+++ app/src/main/java/ru/art241111/graphicaltoolforkawasaki/viewModel/RobotViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import ru.art241111.graphicaltoolforkawasaki.repository.enities.Position
 import ru.art241111.graphicaltoolforkawasaki.repository.RepositoryForRobotApi
 import ru.art241111.graphicaltoolforkawasaki.repository.enities.RobotCommands
+import ru.art241111.graphicaltoolforkawasaki.utils.Delay
 
 class RobotViewModel(application: Application)
     : AndroidViewModel(application) {
@@ -19,4 +20,20 @@ class RobotViewModel(application: Application)
         programList.value = arrayListOf()
         pointList.value = arrayListOf()
     }
+
+    fun createConnection(ip: String, port: Int): Boolean{
+        val repositoryForRobotApi = RepositoryForRobotApi()
+        repositoryForRobotApi.connectToRobotTCP(address = ip, port = port)
+
+        Delay.customDelay(500L)
+
+        return if(repositoryForRobotApi.isConnect()){
+            robot = repositoryForRobotApi
+            true
+        } else{
+            repositoryForRobotApi.disconnect()
+            false
+        }
+    }
+
 }
