8f61fcffcbad84e3e79cfb8fa2a826c7e7d92081
art241111 <art241111@mail.ru>
2020-09-17 14:59:18 +0300

    Fix bug

--- app/src/main/java/ru/art241111/gt_kawasaki/utils/sharedPreferences/JsonHelper.kt
+++ app/src/main/java/ru/art241111/gt_kawasaki/utils/sharedPreferences/JsonHelper.kt
@@ -3,10 +3,7 @@ package ru.art241111.gt_kawasaki.utils.sharedPreferences
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import ru.art241111.gt_kawasaki.utils.entitiesCommandsAndPosition.*
-import ru.art241111.gt_kawasaki.utils.entitiesCommandsAndPosition.commands.CloseGripper
-import ru.art241111.gt_kawasaki.utils.entitiesCommandsAndPosition.commands.Move
-import ru.art241111.gt_kawasaki.utils.entitiesCommandsAndPosition.commands.MoveToPoint
-import ru.art241111.gt_kawasaki.utils.entitiesCommandsAndPosition.commands.RobotCommands
+import ru.art241111.gt_kawasaki.utils.entitiesCommandsAndPosition.commands.*
 
 class JsonHelper {
     fun robotCommandsArrayToJsonString(commands: List<RobotCommands>):  String = commands.joinToString(separator = ";")
@@ -18,7 +15,7 @@ class JsonHelper {
         command.forEach {
             when{
                 it == "CloseGripper" -> returnCommands.add(CloseGripper.parse(it))
-                it == "OpenGripper" -> returnCommands.add(CloseGripper.parse(it))
+                it == "OpenGripper" -> returnCommands.add(OpenGripper.parse(it))
                 it.contains("MoveToPoint", ignoreCase = true)-> returnCommands.add(MoveToPoint.parse(it))
                 it.contains("MOVE", ignoreCase = true)-> returnCommands.add(Move.parse(it))
             }
